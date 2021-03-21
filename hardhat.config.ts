0f31781ced5bda3c7f324db0c2c6c47ea2e26283
Michael DePetrillo <michael.depetrillo@gmail.com>
2021-03-21 12:57:39 -0700

    fix: my version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,11 +2,42 @@ import { HardhatUserConfig } from 'hardhat/types'
 
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
-import '@eth-optimism/plugins/hardhat/compiler';
-import '@eth-optimism/plugins/hardhat/ethers';
+
+import 'dotenv/config'
+import 'hardhat-gas-reporter'
+import '@eth-optimism/plugins/hardhat/compiler'
+import '@eth-optimism/plugins/hardhat/ethers'   
+// import 'hardhat-deploy'
 
 const config: HardhatUserConfig = {
-  solidity: "0.7.3",
+  // defaultNetwork: 'localhost',
+  // namedAccounts: {
+  //   deployer: 0
+  // },
+  solidity: '0.7.6',
+  ovm: {
+    solcVersion: '0.7.6'
+  },
+  // networks: {
+  //   // NOTE: Network names can be named anything
+  //   ethereum_local_L1: {
+  //     url: 'http://127.0.0.1:9545', // EVM L1 Chain
+  //     accounts: {
+  //       mnemonic: 'test test test test test test test test test test test junk'
+  //     },
+  //     tags: ['local', 'L1']
+  //   },
+  //   optimism_local_L2: {
+  //     url: 'http://127.0.0.1:8545', // OVM L2 Chain
+  //     accounts: {
+  //       mnemonic: 'test test test test test test test test test test test junk'
+  //     },
+  //     tags: ['local', 'L2']
+  //   }
+  // },
+  mocha: {
+    timeout: 0,
+  }
 };
 
 export default config
